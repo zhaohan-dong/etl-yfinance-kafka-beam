2a6d46aaa7971f8b154cbebc40ff3caa6bc7bfd0
zhaohan_dong <65422392+zhaohan-dong@users.noreply.github.com>
2023-07-19 22:13:19 +0000

    Added unit test

--- test/kafka_test.py
+++ test/kafka_test.py
@@ -9,6 +9,7 @@ class TestKafkaMessaging(unittest.TestCase):
 
     def test_kafka_messaging(self):
         # Create KafkaProducer
+        value = ""
         producer = KafkaProducer(bootstrap_servers=SERVER_IP, acks="all", value_serializer=(lambda v: json.dumps(v).encode('utf-8')))
 
         # Send a test message
@@ -17,15 +18,18 @@ class TestKafkaMessaging(unittest.TestCase):
         # Create KafkaConsumer
         consumer = KafkaConsumer('yfinance', bootstrap_servers=SERVER_IP, auto_offset_reset='earliest')
 
+        time_count = 0
         # Consume messages from the topic
-        for _ in range(5):  # Try to consume messages for 5 seconds
+        while time_count < 5:  # Try to consume messages for 5 seconds
             for message in consumer:
                 value = json.loads(message.value.decode("utf-8"))  # Decode the value using JSON decoding
                 if value == "Hello":
                     break  # Test message received successfully, exit the loop
             else:
                 time.sleep(1)  # Wait for a second and try again
-        self.fail("Test message not received from Kafka within the timeout")
+                time_count += 1
+        if time_count == 5:
+            self.fail("Test message not received from Kafka within the timeout")
         # Assert that the test message was received
         self.assertEqual(value, "Hello", "Test message not received from Kafka")
 
